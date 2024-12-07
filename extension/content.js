7b2bcea0be0db2f59cd779822aa9b66cd7641764
tymoteusz urban <tymekurban@gmail.com>
2024-12-07 17:58:37 +0100

    change patern matching in predection, fix icon setting

--- extension/content.js
+++ extension/content.js
@@ -1,20 +1,26 @@
-const PREPARED_WEBSITES_URLS = new Map(); // for easy if-else clauses writing
-PREPARED_WEBSITES_URLS.set("https://www.google.com/search", 'google');
+const PREPARED_WEBSITES_URLS = ["https://www.google.com/search*", "https://www.thesun.co.uk/*/"];
 
 // runs pre detection pipeline
 async function runPreDetection() {
 
     const currentUrl = window.location.href;
+    let foundUrl;
 
-    const foundKey = [...PREPARED_WEBSITES_URLS.keys()].find(prefix => currentUrl.startsWith(prefix)); 
-    const foundName = PREPARED_WEBSITES_URLS.get(foundKey) // e.g google
+    for (let i = 0; i < PREPARED_WEBSITES_URLS.length; i++) {
+        let webUrlPattern = PREPARED_WEBSITES_URLS[i];
+        if (matchesPattern(webUrlPattern, currentUrl)) {
+            console.log(`[CLICKGUARD] Matched url: ${webUrlPattern}`)
+            foundUrl = true     
+            break;
+        }
+    }
 
     // if current website is on the list
-    if (typeof foundName !== 'undefined') {
+    if (foundUrl) {
         chrome.storage.sync.get(["searchEngineDetection"]).then((result) => {
             const searchEngineDetection = result["searchEngineDetection"];
             if (searchEngineDetection === true) {
-                sendPreDetectionRequest(foundName);
+                sendPreDetectionRequest();
             } else {
                 console.log("Configured URL was detected, but search engine detection is turned off")
             }
@@ -93,56 +99,25 @@ function addIcons(predictionMap) {
     })
 }
 
-// // adds icons next to the headlines in google search
-// function addIconsGoogle(predictionMap) {
-//     // select all divs with the class 'MjjYud'
-//     console.log('[CLICKGUARD] Adding icons')
-//     const divs = document.querySelectorAll('div.MjjYud');
+function addIcons(predictionMap) {
+    console.log('[CLICKGUARD] Adding icons');
+    const anchors = document.querySelectorAll('a');
+    const processedHrefs = new Set();
 
-//     divs.forEach(div => {
-//         // find all anchor elements inside the div, probably its enough to select only first anchor
-        
-//         const anchors = div.querySelectorAll('a');
-
-//         anchors.forEach(anchor => {
-//             // check if there is an h3 element inside the anchor - page title
-//             const h3Element = anchor.querySelector('h3');
-//             if (h3Element) {
-//                 const hrefValue = anchor.getAttribute('href');
-//                 const prediction = predictionMap.get(hrefValue);
-//                 if (prediction == 1) {
-//                     const svgIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-//                     svgIcon.setAttribute("width", "16");
-//                     svgIcon.setAttribute("height", "16");
-//                     svgIcon.setAttribute("fill", "#dc3545"); // red
-//                     svgIcon.setAttribute("stroke", "#dc3545");
-//                     svgIcon.setAttribute("stroke-width", "1");
-//                     svgIcon.setAttribute("class", "bi bi-exclamation-circle");
-//                     svgIcon.setAttribute("viewBox", "-1 -1 18 18");
-//                     svgIcon.setAttribute("style", "margin-left: 8px")
-//                     svgIcon.innerHTML = `<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-//                                         <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>`
-//                     // insert icon before the h3 text
-//                     h3Element.append(svgIcon);
-//                 } else if (prediction == 0) {
-//                     const svgIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-//                     svgIcon.setAttribute("width", "16");
-//                     svgIcon.setAttribute("height", "16");
-//                     svgIcon.setAttribute("fill", "#198754"); // green
-//                     svgIcon.setAttribute("stroke", "#198754");
-//                     svgIcon.setAttribute("stroke-width", "1");
-//                     svgIcon.setAttribute("class", "bi bi-check-circle");
-//                     svgIcon.setAttribute("viewBox", "-1 -1 18 18");
-//                     svgIcon.setAttribute("style", "margin-left: 8px")
-//                     svgIcon.innerHTML = `<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-//                                         <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>`
-//                     // insert icon before the h3 text
-//                     h3Element.append(svgIcon);                
-//                 }
-//             }
-//         });
-//     });
-// }
+    anchors.forEach(anchor => {
+        const hrefValue = anchor.getAttribute('href');
+        // skip if href has already been processed
+        if (processedHrefs.has(hrefValue)) {
+            return; // forEach is a function - skips to the next iteration
+        }
+        const prediction = predictionMap.get(hrefValue);
+        if (typeof prediction !== 'undefined') {
+            const icon = createIcon(prediction);
+            anchor.append(icon);
+        }
+        processedHrefs.add(hrefValue);
+    });
+}
 
 runPreDetection();
 
