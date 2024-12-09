ef5d1d6a2163f130b3b38e89850d2138e241d275
Mateusz <mateusz.kubita6@gmail.com>
2024-12-09 20:34:17 +0100

    fix bug final moels

--- notebooks/final_models.py
+++ notebooks/final_models.py
@@ -17,6 +17,25 @@ client = OpenAI(
     api_key=api_key,
 )
 
+metrics_functions = {
+    "n_words": measure.words_count,
+    "cw_percentage": measure.common_words_ratio,
+    "capitals_ratio": measure.capital_letters_ratio,
+    "capitals_count":measure.capital_words_count,
+    "bait_punct": measure.clickbait_punctuation_count,
+    "nonbait_punct": measure.nonclickbait_punctuation_count,
+    "numbers": measure.numbers_count,
+    "2nd_pronouns": measure.pronouns_2nd_person_count,
+    "superlatives": measure.superlatives_ratio,
+    "speculatives": measure.speculatives_count,
+    "bait_words": measure.baiting_words_count,
+    "polarity": measure.polarity_score,
+    "subjectivity": measure.subjectivity_score,
+    "fres": measure.flesch_reading_ease_score,
+    "batness_measures": baitness_measure,
+}
+
+
 
 def return_embeddings_chat(prompt):
     res = requests.post(f"https://api.openai.com/v1/embeddings",
