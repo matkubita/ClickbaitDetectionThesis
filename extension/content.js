938fa553bbcd945f29a70aa9f33df9dde41fd1f2
tymoteusz urban <tymekurban@gmail.com>
2024-12-12 00:10:47 +0100

    yellow icon for preclick detection

--- extension/content.js
+++ extension/content.js
@@ -84,13 +84,19 @@ function createIcon(prediction) {
     svgIcon.setAttribute("stroke-width", "1");
     svgIcon.setAttribute("viewBox", "-1 -1 18 18");
     svgIcon.setAttribute("style", "margin-left: 8px")
-    if (prediction == 1) {
+    if (prediction >= 0.6) {
         svgIcon.setAttribute("fill", "#dc3545"); // red
         svgIcon.setAttribute("stroke", "#dc3545");
         svgIcon.setAttribute("class", "bi bi-exclamation-circle");
         svgIcon.innerHTML = `<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                             <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>`
-    } else if (prediction == 0) {
+    } else if (prediction >= 0.4) {
+        svgIcon.setAttribute("fill", "#e9d502"); // yellow
+        svgIcon.setAttribute("stroke", "#e9d502");
+        svgIcon.setAttribute("class", "bi bi-exclamation-triangle");
+        svgIcon.innerHTML = `<path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.15.15 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.2.2 0 0 1-.054.06.1.1 0 0 1-.066.017H1.146a.1.1 0 0 1-.066-.017.2.2 0 0 1-.054-.06.18.18 0 0 1 .002-.183L7.884 2.073a.15.15 0 0 1 .054-.057m1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767z"/>
+                            <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>`
+    } else if (prediction >= 0) {
         svgIcon.setAttribute("fill", "#198754"); // green
         svgIcon.setAttribute("stroke", "#198754");
         svgIcon.setAttribute("class", "bi bi-check-circle");
